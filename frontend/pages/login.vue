f011bc80b9d56452ae5edbc3bb73e3d4530320d8
Ade Kurniawan <57614206+adekurniawan22@users.noreply.github.com>
2024-01-10 20:13:18 +0700

    pages register update

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -2,10 +2,9 @@
     <v-row justify="center">
         <v-col cols="10" md="6">
             <v-card>
-                <v-toolbar color="primary" dark> Register </v-toolbar>
+                <v-toolbar color="primary" dark> Login </v-toolbar>
                 <v-card-text>
                     <v-form ref="form">
-                        <v-text-field v-model="form.fullname" label="Fullname" :rules="rules.fullname" required></v-text-field>
                         <v-text-field
                             v-model="form.email"
                             label="Email"
@@ -29,32 +28,16 @@
                                 >
                             </template>
                         </v-text-field>
-                        <v-text-field
-                            v-model="form.password_confirmation"
-                            label="Confirm Password"
-                            :rules="rules.password_confirmation"
-                            :type="passwordConfirmationFieldType"
-                            required
-                        >
-                            <template v-slot:append>
-                                <v-icon
-                                    class="password-toggle-icon"
-                                    @mousedown="togglePasswordVisibility(true, 'password_confirmation')"
-                                    @mouseup="togglePasswordVisibility(false, 'password_confirmation')"
-                                    >{{ passwordConfirmationIcon }}</v-icon
-                                >
-                            </template>
-                        </v-text-field>
                     </v-form>
                 </v-card-text>
                 <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn :loading="isLoading" color="primary" @click="onSubmit">Register</v-btn>
+                    <v-btn :loading="isLoading" color="primary" @click="onSubmit">Login</v-btn>
                 </v-card-actions>
             </v-card>
             <div class="d-flex align-baseline">
-                <p>Kamu sudah punya akun?</p>
-                <v-btn text plain :ripple="false" class="pl-2" to="/login" color="primary">Login</v-btn>
+                <p>Kamu belum punya akun?</p>
+                <v-btn text plain :ripple="false" class="pl-2" to="/register" color="primary">Register</v-btn>
             </div>
         </v-col>
     </v-row>
@@ -66,17 +49,12 @@ export default {
     data() {
         return {
             isLoading: false,
-            emailExist: false,
             form: {
-                fullname: "",
                 email: "",
                 password: "",
-                password_confirmation: "",
             },
             passwordVisible: false,
-            passwordConfirmationVisible: false,
             rules: {
-                fullname: [(v) => !!v || "Fullname is required"],
                 email: [
                     (v) => !!v || "E-mail is required",
                     (v) => /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/.test(v) || "E-mail must be valid",
@@ -85,10 +63,6 @@ export default {
                 password: [
                     (v) => !!v || "Password is required",
                     (v) => v.length >= 6 || "Password must be at least 6 characters",
-                ],
-                password_confirmation: [
-                    (v) => !!v || "Confirm password is required",
-                    (v) => v === this.form.password || "Passwords do not match",
                 ],
             },
         };
@@ -99,12 +73,6 @@ export default {
         },
         passwordIcon() {
             return this.passwordVisible ? "mdi-eye" : "mdi-eye-off";
-        },
-        passwordConfirmationFieldType() {
-            return this.passwordConfirmationVisible ? "text" : "password";
-        },
-        passwordConfirmationIcon() {
-            return this.passwordConfirmationVisible ? "mdi-eye" : "mdi-eye-off";
         },
     },
     methods: {
